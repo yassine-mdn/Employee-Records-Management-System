f650ab958744ced3ad5a6a61c6b182b0c21f6921
yassine <yassinemouddene@gmail.com>
2025-01-14 17:52:20 +0100

    feat: generated spring Application

---
+++ ermc-back/src/test/java/com/erms/back/ErmcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erms.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErmcBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

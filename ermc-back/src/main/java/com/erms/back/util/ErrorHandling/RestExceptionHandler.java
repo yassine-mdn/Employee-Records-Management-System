42c7da3e49c86b36e14cfbcb4b4f741b60118de7
yassine <yassinemouddene@gmail.com>
2025-01-15 21:04:14 +0100

    feat: Expanded on Custom Exception Handling

--- ermc-back/src/main/java/com/erms/back/util/ErrorHandling/RestExceptionHandler.java
+++ ermc-back/src/main/java/com/erms/back/util/ErrorHandling/RestExceptionHandler.java
@@ -3,16 +3,27 @@ package com.erms.back.util.ErrorHandling;
 import com.erms.back.Exception.BaseAppException;
 import com.erms.back.Exception.EmployeeNotFoundException;
 import com.erms.back.Exception.PageOutOfBoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -20,12 +31,51 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
         ApiError apiError = new ApiError(NOT_FOUND);
         apiError.setMessage(ex.getMessage());
+        apiError.setDebugMessage(ex.getClass().getSimpleName());
         return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(BaseAppException.class)
-    public ResponseEntity<Object> handleBaseException(BaseAppException baseAppException, HttpStatus status, WebRequest request, HttpHeaders headers ) {
+    public ResponseEntity<Object> handleBaseException(BaseAppException baseAppException) {
         return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, baseAppException.getMessage(), baseAppException));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex));
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        apiError.setDebugMessage(ex.getTitleMessageCode());
+        List<ApiSubError> subErrors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String objectName = error.getObjectName();
+            Object rejectedValue = ((FieldError) error).getRejectedValue();
+            String errorMessage = error.getDefaultMessage();
+           subErrors.add(new ApiValidationError(objectName, fieldName, rejectedValue, errorMessage));
+        });
+        apiError.setSubErrors(subErrors);
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+
+        String errorMessage = ex.getMessage();
+        String stopPattern = "[insert";
+        int stopIndex = errorMessage.indexOf(stopPattern);
+        String cleanedMessage = stopIndex != -1 ? errorMessage.substring(0, stopIndex).trim() : errorMessage;
+
+
+        ApiError apiError = new ApiError(NOT_FOUND);
+        apiError.setMessage(cleanedMessage);
+        apiError.setDebugMessage(ex.getClass().getSimpleName());
+        return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(EmployeeNotFoundException.class)

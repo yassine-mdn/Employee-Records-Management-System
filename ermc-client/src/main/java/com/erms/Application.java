bfd14934864b100d7d78fbe7a00d476ade41d329
yassine <yassinemouddene@gmail.com>
2025-01-17 20:57:06 +0100

    feat: windows appears on screen

---
+++ ermc-client/src/main/java/com/erms/Application.java
@@ -0,0 +1,30 @@
+package com.erms;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JFrame {
+    public Application() {
+        init();
+    }
+
+    private void init() {
+        setTitle("FlatLaf Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(new Dimension(1200, 700));
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        FlatRobotoFont.install();
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        FlatMacDarkLaf.setup();
+        EventQueue.invokeLater(() -> new Application().setVisible(true));
+    }
+
+}
